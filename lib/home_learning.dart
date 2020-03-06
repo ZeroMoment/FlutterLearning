47e69a4b1ad1fc44fcf7d6d2efd405e746cbec2b
lizhiming <lizm1103@163.com>
2020-03-06 17:56:02 +0400

    stream

--- lib/home_learning.dart
+++ lib/home_learning.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_learning/route/routes.dart' as mRoutes;
 
@@ -12,20 +14,77 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  bool isDataInited = false;
   List<String> _list = List();
+
+  StreamController<int> cr = new StreamController();
+  int _clickCount = 0;
+
+  StreamController<int> cr2 = new StreamController();
+  final st = StreamTransformer<int, String>.fromHandlers(
+    handleData: (int data, sink) {
+      if(data == 20) {
+        sink.add('添加正常');
+      } else {
+        sink.addError('添加错误');
+      }
+    }
+  );
+
+  StreamController<int> cr3 = new StreamController();
 
   @override
   void initState() {
     super.initState();
     print("init state......");
+
+    _testStream();
+    _stream();
+
     _list..add("Animation")
       ..add("Shared")
       ..add("MethodChannel");
 
-    setState(() {
-      isDataInited = true;
+  }
+
+  void _testStream() {
+    cr.stream.listen((data) {
+      debugPrint('点击：$data');
     });
+
+    cr2.stream.transform(st).listen((String data){
+      debugPrint(data);
+    }).onError((e){
+      debugPrint('$e');
+    });
+
+    cr2.sink.add(11);
+    cr2.sink.add(20);
+    cr2.close();
+
+    cr3.stream.where((value) {
+      return value == 33;
+    }).listen((data) {
+      debugPrint('data:$data');
+    });
+
+    cr3.sink.add(222);
+    cr3.sink.add(33);
+    cr3.close();
+
+
+    Stream<String>.fromIterable(['a', 'b', 'c']).listen((data) {
+      debugPrint('ite:$data');
+    });
+  }
+
+  void _stream() async {
+    Duration interval = Duration(seconds: 1);
+    Stream<int> stream = Stream<int>.periodic(interval, (data) => data);
+    stream = stream.take(6);
+    stream = stream.map((data) => data + 3);
+    await for (int i in stream) {
+      debugPrint('hehe:$i');
+    }
   }
 
   @override
@@ -35,8 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: isDataInited ?
-        ListView.builder(
+        body: ListView.builder(
             itemCount: _list.length,
             itemExtent: 50.0,
             itemBuilder: (BuildContext context, int index) {
@@ -47,9 +105,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
               );
             }
-        ) : Center(
-          child: Text("加载中..."),
-        )
+        ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _addStearm,
+      ),
     );
   }
 
@@ -61,5 +121,16 @@ class _MyHomePageState extends State<MyHomePage> {
     } else if("MethodChannel" == itemTxt) {
       Navigator.pushNamed(context, mRoutes.methodChannelPage);
     }
+  }
+
+  void _addStearm() {
+    _clickCount++;
+    cr.sink.add(_clickCount);
+  }
+
+  @override
+  void dispose() {
+    cr.close();
+    super.dispose();
   }
 }
